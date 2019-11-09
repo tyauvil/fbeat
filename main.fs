eeb39deb77db223d0b73cf2aef87caaddefc072e
ty <ty.auvil@gmail.com>
2019-11-09 16:19:42 -0700

    add beats function with warbler

--- main.fs
+++ main.fs
@@ -1,9 +1,26 @@
-open Giraffe
+open System
+
 open Microsoft.AspNetCore.Builder
 open Microsoft.Extensions.DependencyInjection
 open Microsoft.AspNetCore.Hosting
 
-let webApp = route "/" >=> Successful.OK "Hello World"
+open Giraffe
+
+let beats() =
+    let utc = 
+        (DateTime.UtcNow.AddHours(1.0).TimeOfDay.TotalSeconds
+        |> float) * 0.011_574 
+        |> int
+
+
+    let prefix = match utc with
+                 | i when i < 10 -> "00"
+                 | i when i < 100 -> "0"
+                 | _ -> ""
+    
+    sprintf "@%s%i.beats" prefix utc
+
+let webApp = route "/" >=> warbler (fun _ -> text (beats()))
 
 let configureApp (app : IApplicationBuilder) =
     // Add Giraffe to the ASP.NET Core pipeline
