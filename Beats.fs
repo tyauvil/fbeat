671a6a14d81142d26fe1b9acf886a758baedcb3a
ty <ty.auvil@gmail.com>
2019-11-09 23:11:52 -0700

    format files

--- Beats.fs
+++ Beats.fs
@@ -3,15 +3,12 @@ module Beats
 open System
 
 let beats() =
-    let utc = 
-        (DateTime.UtcNow.AddHours(1.0).TimeOfDay.TotalSeconds
-        |> float) * 0.011_574 
-        |> int
+    let utc = (DateTime.UtcNow.AddHours(1.0).TimeOfDay.TotalSeconds |> float) * 0.011_574 |> int
 
+    let prefix =
+        match utc with
+        | i when i < 10 -> "00"
+        | i when i < 100 -> "0"
+        | _ -> ""
 
-    let prefix = match utc with
-                 | i when i < 10 -> "00"
-                 | i when i < 100 -> "0"
-                 | _ -> ""
-    
     sprintf "@%s%i.beats" prefix utc
